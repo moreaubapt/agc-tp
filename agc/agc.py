383bcdd2753ec0d4b66bce43dee41dc65b544924
moreaubapt <moreaubapt@eisti.eu>
2021-01-04 15:45:52 +0100

    partie 2

--- agc/agc.py
+++ agc/agc.py
@@ -23,13 +23,13 @@ from collections import Counter
 # ftp://ftp.ncbi.nih.gov/blast/matrices/
 import nwalign3 as nw
 
-__author__ = "Your Name"
+__author__ = "Moreau Baptiste"
 __copyright__ = "Universite Paris Diderot"
-__credits__ = ["Your Name"]
+__credits__ = ["Moreau Baptiste"]
 __license__ = "GPL"
 __version__ = "1.0.0"
-__maintainer__ = "Your Name"
-__email__ = "your@email.fr"
+__maintainer__ = "Moreau Baptiste"
+__email__ = "moreaubapt@eisti.eu"
 __status__ = "Developpement"
 
 
@@ -93,12 +93,23 @@ def read_secquence(amplicon_file):
                 yield seq
 
 def read_fasta(amplicon_file, minseqlen):
+    """
+    prend deux arguments correspondant au fichier fasta.gz et à la longueur minimale
+    des séquences et retourne un générateur de séquences de longueur l >= minseqlen: yield sequence.
+    """
     sequencer = read_secquence(amplicon_file)
     for seq in sequencer:
         if len(seq) >= minseqlen:
             yield seq
 
 def dereplication_fulllength(amplicon_file, minseqlen, mincount):
+    """
+    Prend trois arguments correspondant au fichier fasta,  la longueur minimale des séquences
+    et leur comptage minimum. Elle fait appel au générateur fourni par read_fasta
+    et retourne un générateur des séquences uniques ayant une occurrence O>=mincount
+    ainsi que leur occurrence. Les séquences seront retournées par ordre décroissant d’occurrence:
+    yield [sequence, count]
+    """
     sequences = read_fasta(amplicon_file, minseqlen)
     dic = {}
     for i,seq in enumerate(sequences):
@@ -113,6 +124,10 @@ def dereplication_fulllength(amplicon_file, minseqlen, mincount):
             yield [key,dic[key]]
 
 def get_chunks(sequence, chunk_size):
+    """
+    prend une séquence et une longueur de segment l: chunk_size et retourne une liste de sous-séquences de taille l non chevauchantes. A minima 4 segments doivent être obtenus par séquence.
+
+    """
     l = [None,None,None,None]
     if len(sequence) >= chunk_size*4:
         for i in range(len(l)):
@@ -136,19 +151,80 @@ def cut_kmer(read, kmer_size):
         yield read[i:i+kmer_size]
 
 def get_identity(alignment_list):
-    #al = nw.global_align(alignment_list[0], alignment_list[1], gap_open=-1, gap_extend=-1, matrix=os.path.abspath(os.path.join(os.path.dirname(__file__),"MATCH")))
-    #print(al)
-    countDiff = 0
-    tot = 0
-    for al in alignment_list:
-        countDiff+=al.count("-")
-        tot = len(al)
-    ident = (tot-(countDiff))
-    return ident/tot
+    """prend un alignement (sous forme de liste) et calcule le pourcentage d’identité entre
+    es deux séquences selon la formule: id = nb nucléotides identiqueslongueur de l'alignement
+    """
+    total = len(alignment_list[0])
+    identik = 0
+    for i in range(total):
+        if alignment_list[0][i] == alignment_list[1][i]:
+            identik +=1
+    return 100*(identik/total)
+
 def chimera_removal(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
-    sequences = dereplication_fulllength(amplicon_file, minseqlen, mincount)
-    for sequence in sequences:
-        segments = get_chunks(sequence, chunk_size)
+    """Fait appel au générateur fourni par dereplication_fulllength et
+    retourne un générateur des séquences non chimérique au format:
+    yield [sequence, count]
+    """
+    kmer_dict = {}
+    perc_identity_matrix = []
+    chunk_match = []
+    seq_list = []
+    chim_id = 0
+    for i, occurence_list in enumerate(list(dereplication_fulllength(amplicon_file, minseqlen,
+                                                                     mincount))):
+        chim = True
+        chunk_list = get_chunks(occurence_list[0], chunk_size)
+        for chunk in chunk_list:
+            chunk_match.append([i[0] for i in Counter([ids for kmer in cut_kmer(chunk, kmer_size) if kmer in kmer_dict for ids in kmer_dict[kmer]]).most_common(8)])
+        com_seq = common(chunk_match[0], chunk_match[1])
+        for j in range(2, len(chunk_match)):
+            com_seq = common(com_seq, chunk_match[j])
+        if len(com_seq) > 1:
+            for k in range(len(chunk_list)):
+                perc_identity_matrix.append([][k])
+            for seq in com_seq[0:2]:
+                seq_chunk_list = get_chunks(seq_list[seq], chunk_size)
+                for l, chunk in enumerate(chunk_list):
+                    perc_identity_matrix[l].append(get_identity(
+                        nw.global_align(chunk, seq_chunk_list[l],
+                            gap_open = -1, gap_extend = 1, matrix = "MATCH")))
+            std_list = []
+            flag_file = 0
+            flag_similarity = 0
+            for line in perc_identity_matrix:
+                std_list.append(statistics.stdev([line[0], line[1]]))
+                if flag_file == 0:
+                    val0 = line[0]
+                    val1 = line[0]
+                    flag_file = 1
+                else:
+                    if flag_similarity == 1:
+                        continue
+                    if val0 != line[0] and val1 != line[1]:
+                        flag_similarity = 1
+                    val0 = line[0]
+                    val1 = line[0]
+            std_mean = statistics.mean(std_list)
+            if std_mean > 5 and flag_similarity == 1:
+                chim = True
+            chim =  False
+
+
+
+        else:
+            chim = False
+        if not chim:
+            #kmer_dict = get_unique_kmer(kmer_dict, occurence_list[0], chim_id, kmer_size)
+            for kmer in cut_kmer(occurence_list[0],kmer_size):
+                if kmer not in kmer_dict:
+                    kmer_dict[kmer] = [chim_id]
+                else :
+                    kmer_dict[kmer].append(chim_id)
+                    kmer_dict[kmer] = list(set(kmer_dict[kmer]).union(set(kmer_dict[kmer])))
+            seq_list.append(occurence_list[0])
+            chim_id += 1
+            yield occurence_list
 
 def abundance_greedy_clustering(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
     pass
